2cd37546f081ac912a00778970deeec72f5f7b7b
Ryan-HK <ryan9037hk@gmail.com>
2022-11-06 07:51:52 +0900

    22/11/06 Study Spring - Make MVC Framework

--- Spring_MVC/servlet/src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
+++ Spring_MVC/servlet/src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -6,7 +6,11 @@ import hello.servlet.web.frontcontroller.v3.ControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import hello.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
 import hello.servlet.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
+import hello.servlet.web.frontcontroller.v5.adapter.ControllerV4HandlerAdapter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -30,42 +34,55 @@ public class FrontControllerServletV5 extends HttpServlet {
         initHandlerAdapters();
     }
 
-    private boolean initHandlerAdapters() {
-        return handlerAdapters.add(new ControllerV3HandlerAdapter());
+    private void initHandlerAdapters() {
+        handlerAdapters.add(new ControllerV3HandlerAdapter());
+        handlerAdapters.add(new ControllerV4HandlerAdapter());
     }
 
     private void initHandlerMappingMap() {
+        //V3 HandlerMapping
         handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
         handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
         handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+
+        //V4 HandlerMapping
+        handlerMappingMap.put("/front-controller/v5/v4/members/new-form", new MemberFormControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members/save", new MemberSaveControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members", new MemberListControllerV4());
     }
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("FrontControllerServletV5.service");
 
+        //Step.1 : Client의 RequestURI를 통해, handlerMappingMap에 저장 되어 있는 handler (Controller)
         Object handler = getHandler(request);
 
+        //Step.2 : Step.1에서 Client의 Request를 처리할 Handler (Controller)가 없다면 404 NOT FOUND
         if(handler == null){
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             return;
         }
 
-        MyHandlerAdapter adapter = getAdapter(handler);
+        //Step.3 : Step.1에서 얻은 handler를 통해, 이를 실행시킬 MyHandlerAdapter 얻기
+        // -. HandlerAdapter는 List객체인 HandlerAdapters에 저장되어 있음.
+        // -. List객체를 순회하며, Adapter가 handler를 실행시킬 수 있는 지 판별 -> adapter.supports(handler)
+        MyHandlerAdapter adapter = getHandlerAdapter(handler);
+
+        //Step.4 : Step.3에서 얻은 Adapter를 통해, Controller 실행하기
+        // -. 비즈니즈 로직의 산출물 생성
         ModelView mv = adapter.handle(request, response, handler);
 
+        //Step.5 : ViewResoulver를 통해, View를 생성
         String viewName = mv.getViewName();// 논리이름
         MyView view = viewResolver(viewName);
 
+        //Step.6 : 생성 된 View에 Model객체를 전달
         view.render(mv.getModel(),request, response);
     }
 
     private static MyView viewResolver(String viewName) {
         return new MyView("/WEB-INF/views/" + viewName + ".jsp");
-    }
-
-    private MyHandlerAdapter getAdapter(Object handler) {
-        return getHandlerAdapter(handler);
     }
 
     private MyHandlerAdapter getHandlerAdapter(Object handler) {
