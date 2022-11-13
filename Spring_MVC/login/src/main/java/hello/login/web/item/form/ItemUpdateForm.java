e8f88ce7ecce8eaa528e4e6a03a495531b1270d0
Ryan-HK <ryan9037hk@gmail.com>
2022-11-13 20:30:12 +0900

    22/11/13 Study Spring - Login Start

--- Spring_MVC/login/src/main/java/hello/login/web/item/form/ItemUpdateForm.java
+++ Spring_MVC/login/src/main/java/hello/login/web/item/form/ItemUpdateForm.java
@@ -19,7 +19,7 @@ public class ItemUpdateForm {
     @Range(min = 1000, max = 1000000)
     private Integer price;
 
-    //수정에서는 수량은 자유롭게 변경할 수 있다.
+    //수정에서는 수량은 자유롭게 변경할 수 있다
     private Integer quantity;
 
 }
