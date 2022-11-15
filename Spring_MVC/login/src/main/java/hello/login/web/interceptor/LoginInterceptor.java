fac3127b8cea667f08552342a56a0a0a23088c10
Ryan-HK <ryan9037hk@gmail.com>
2022-11-16 03:01:54 +0900

    22/11/16 Study Spring - Login Filter and Interceptor

---
+++ Spring_MVC/login/src/main/java/hello/login/web/interceptor/LoginInterceptor.java
@@ -0,0 +1,55 @@
+package hello.login.web.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
+@Slf4j
+public class LoginInterceptor implements HandlerInterceptor {
+
+
+    public static final String LOG_ID = "logId";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        String requestURI = request.getRequestURI();
+        String uuid = UUID.randomUUID().toString();
+
+        request.setAttribute(LOG_ID, uuid);
+
+        //@RequestMapping : HandlerMethod
+        //정적 리소스 : ResourceHttpRequestHandler
+        if (handler instanceof HandlerMethod) {
+            //호출할 컨트롤러 메서드의 모든 정보가 포함되어 있다.
+            HandlerMethod hm = (HandlerMethod) handler;
+        }
+
+        log.info("REQUEST [{}][{}][{}]", uuid, requestURI, handler);
+
+        //preHandle(..)의 return값이 true면, Controller를 호출한다.
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("postHandle [{}]", modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        String requestURI = request.getRequestURI();
+        String logId = (String) request.getAttribute(LOG_ID);
+
+        log.info("RESPONSE [{}][{}][{}]", logId, requestURI, handler);
+
+        if (ex != null) {
+            log.error("afterCompletion error!!", ex);
+        }
+    }
+}
