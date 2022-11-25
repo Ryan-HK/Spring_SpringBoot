b5040e4679de0f81ecbac2de0b484bf19c0e95cb
Ryan-HK <ryan9037hk@gmail.com>
2022-11-25 23:55:36 +0900

    22/11/25 Study Spring - JDBC

---
+++ Spring_DB_Basic/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -0,0 +1,69 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.connection.DBConnectionUtil;
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.*;
+
+/**
+ * JDBC - DriverManager 사용
+ */
+@Slf4j
+public class MemberRepositoryV0 {
+
+    private Member save(Member member) throws SQLException {
+        String sql = "insert into member(member_id, money) values (?, ?)";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            PreparedStatement preparedStatement = con.prepareStatement(sql);
+            pstmt.setString(1, member.getMemberId());
+            pstmt.setInt(2, member.getMoney());
+            pstmt.executeUpdate();
+            return member;
+        } catch (SQLException e) {
+            log.error("db error, e");
+            throw e;
+        } finally {
+            close(con, pstmt, null);
+        }
+
+
+    }
+
+    private void close(Connection con, Statement stmt, ResultSet resultSet) {
+
+        if (rs != null) {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+
+
+        if (stmt != null) {
+            try {
+                stmt.close();   //SQLException
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+    }
+
+    private Connection getConnection() {
+        return DBConnectionUtil.getConnection();
+    }
+}
