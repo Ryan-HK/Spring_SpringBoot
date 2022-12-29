2ee9576f39bfa673b3739a9ff4129a5ff5451eed
Ryan-HK <ryan9037hk@gmail.com>
2022-12-29 23:34:07 +0900

    22/12/29 Study Spring - JPA and Spring Data JPA

--- Spring_DB_Advance/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ Spring_DB_Advance/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -12,7 +12,8 @@ import org.springframework.context.annotation.Profile;
 //@Import(MemoryConfig.class)
 //@Import(JdbcTemplateV1Config.class)
 //@Import(JdbcTemplateV2Config.class)
-@Import(MyBatisConfig.class)
+//@Import(MyBatisConfig.class)
+@Import(JpaConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
