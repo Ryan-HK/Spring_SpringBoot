32c5dc2a1035ece357f9f98fab1e333ec7db1cff
Ryan <ryan9037hk@gmail.com>
2023-05-01 07:27:01 +0900

    23/05/01 Study SpringAdvance - Strategy Pattern Apply

--- Spring_Advance/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ Spring_Advance/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -1,6 +1,7 @@
 package hello.advanced.trace.strategy;
 
 import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.Strategy;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
 import hello.advanced.trace.template.code.AbstractTemplate;
@@ -55,6 +56,84 @@ public class ContextV1Test {
         StrategyLogic2 strategyLogic2 = new StrategyLogic2();
         ContextV1 context2 = new ContextV1(strategyLogic2);
         context2.execute();
+    }
+
+    /**
+     * 익명 내부 클래스를 사용한 전략 패턴
+     */
+    @Test
+    void strategyV2() {
+        Strategy strategyLogic1 = new Strategy() {
+
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+
+        };
+
+        ContextV1 context1 = new ContextV1(strategyLogic1);
+        context1.execute();
+
+        Strategy strategyLogic2 = new Strategy() {
+
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+
+        };
+
+        ContextV1 context2 = new ContextV1(strategyLogic2);
+        context2.execute();
+
+    }
+
+
+    /**
+     * 익명 내부 클래스를 사용한 전략 패턴
+     */
+    @Test
+    void strategyV3() {
+
+
+        ContextV1 context1 = new ContextV1(new Strategy() {
+
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+
+        });
+
+        context1.execute();
+
+        ContextV1 context2 = new ContextV1(new Strategy() {
+
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+
+        });
+
+        context2.execute();
+
+    }
+
+    /**
+     * Lambda Expression을 사용한 전략 패턴
+     */
+    @Test
+    void strategyV4() {
+
+
+        ContextV1 context1 = new ContextV1(() -> log.info("비즈니스 로직1 실행"));
+        context1.execute();
+
+        ContextV1 context2 = new ContextV1(() -> log.info("비즈니스 로직2 실행"));
+        context2.execute();
+
     }
 
 }
