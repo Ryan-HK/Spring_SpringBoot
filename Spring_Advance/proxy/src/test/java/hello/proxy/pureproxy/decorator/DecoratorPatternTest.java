e9b01867b5e854393043ba6f93b7caf235667213
Ryan <ryan9037hk@gmail.com>
2023-05-06 04:49:15 +0900

    23/05/06 Study SpringAdvance - DecoratorPattern

--- Spring_Advance/proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ Spring_Advance/proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -1,9 +1,7 @@
 package hello.proxy.pureproxy.decorator;
 
 
-import hello.proxy.pureproxy.decorator.code.Component;
-import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
-import hello.proxy.pureproxy.decorator.code.RealComponent;
+import hello.proxy.pureproxy.decorator.code.*;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -15,6 +13,23 @@ public class DecoratorPatternTest {
     void noDecorator() {
         Component realComponent = new RealComponent();
         DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
+        client.execute();
+    }
+
+    @Test
+    void decorator1() {
+        Component realComponent = new RealComponent();
+        Component messageComponent = new MessageDecorator(realComponent);
+        DecoratorPatternClient client = new DecoratorPatternClient(messageComponent);
+        client.execute();
+    }
+
+    @Test
+    void decorator2() {
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
+        Component timeDecorator = new TimeDecorator(messageDecorator);
+        DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
         client.execute();
     }
 
