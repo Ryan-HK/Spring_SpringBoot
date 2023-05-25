d5ffe8259fece0c38e97628cb217cab380002417
Ryan <ryan9037hk@gmail.com>
2023-05-25 22:06:51 +0900

    23/05/25 Study SpringAdvance - BeanPostProcessor apply

--- Spring_Advance/proxy/src/test/java/hello/proxy/postprocessor/BasicTest.java
+++ Spring_Advance/proxy/src/test/java/hello/proxy/postprocessor/BasicTest.java
@@ -3,7 +3,9 @@ package hello.proxy.postprocessor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,23 +14,28 @@ public class BasicTest {
 
     @Test
     void basicConfig() {
-        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(BeanPostProcessorConfig.class);
 
-        //A는 빈으로 등록된다.
-        A a = applicationContext.getBean("beanA", A.class);
-        a.helloA();
+        //beanA 이름으로 B 객체가 빈으로 등록된다.
+        B b = applicationContext.getBean("beanA", B.class);
+        b.helloB();
 
-        //B는 빈으로 등록되지 않는다.
-        Assertions.assertThrows(NoSuchBeanDefinitionException.class, () -> applicationContext.getBean(B.class));
+        //A는 빈으로 등록되지 않는다.
+        Assertions.assertThrows(NoSuchBeanDefinitionException.class, () -> applicationContext.getBean(A.class));
     }
 
     @Slf4j
     @Configuration
-    static class BasicConfig {
+    static class BeanPostProcessorConfig {
 
         @Bean(name = "beanA")
         public A a() {
             return new A();
+        }
+
+        @Bean
+        public AToBPostProcessor helloPostProcessor() {
+            return new AToBPostProcessor();
         }
     }
 
@@ -47,6 +54,22 @@ public class BasicTest {
             log.info("hello B");
         }
     }
+
+    @Slf4j
+    static class AToBPostProcessor implements BeanPostProcessor {
+
+        @Override
+        public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+            log.info("beanName = {}, bean = {}", beanName, bean);
+
+            if (bean instanceof A) {
+                return new B();
+            }
+
+            return bean;
+        }
+    }
+
 
 
 
