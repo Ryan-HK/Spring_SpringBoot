dc6e34f29009f499ca504657b45cfd5df1063417
Ryan <ryan9037hk@gmail.com>
2024-04-07 19:24:25 +0900

    24/04/07 Study SpringSecurity - Remember-Me

--- Spring_Security_StudyV2/springsecuritymaster/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ Spring_Security_StudyV2/springsecuritymaster/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -22,7 +22,16 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults());
+                .formLogin(Customizer.withDefaults())
+                .rememberMe(rememberMe -> rememberMe
+//                        .alwaysRemember(true)
+                        .tokenValiditySeconds(3600)
+                        .userDetailsService(userDetailsService())
+                        .rememberMeParameter("remember")
+                        .rememberMeCookieName("remember")
+                        .key("security")
+                )
+        ;
         return http.build();
     }
 
