fadd45ea361e7d0605f7bb2cbd34c183424afb3c
Ryan <ryan9037hk@gmail.com>
2024-04-09 04:16:22 +0900

    24/04/09 Study SpringSecurity - AuthenticationManager

--- Spring_Security_StudyV2/springsecuritymaster/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ Spring_Security_StudyV2/springsecuritymaster/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -8,7 +8,9 @@ import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.Authentication;
@@ -19,6 +21,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
@@ -34,60 +37,22 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        HttpSessionRequestCache requestCache = new HttpSessionRequestCache();
-        requestCache.setMatchingRequestParameterName("customParam=y");
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();            // build() 는 최초 한번 만 호출해야 한다
 
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/logoutSuccess").permitAll()
+                        .requestMatchers("/","/api/login").permitAll()
                         .anyRequest().authenticated())
-                .formLogin(form -> form.successHandler(new AuthenticationSuccessHandler() {
-                    @Override
-                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                        SavedRequest savedRequest = requestCache.getRequest(request, response);
-                        String redirectUrl = savedRequest.getRedirectUrl();
-                        response.sendRedirect(redirectUrl);
-                    }
-                })).requestCache(cache -> cache.requestCache(requestCache));
-
-
-            //TODO : logout
-//                .logout(logout -> logout
-//                        .logoutUrl("/logoutProc")
-//                        .logoutRequestMatcher(new AntPathRequestMatcher("logoutProc", "GET"))
-//                        .logoutSuccessUrl("/logoutSuccess")
-//                        .logoutSuccessHandler(new LogoutSuccessHandler() {
-//                            @Override
-//                            public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-//                                response.sendRedirect("/logoutSuccess");
-//                            }
-//                        })
-//                        .deleteCookies("JSESSIONID", "remember-me")
-//                        .invalidateHttpSession(true)
-//                        .addLogoutHandler(new LogoutHandler() {
-//                            @Override
-//                            public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-//                                HttpSession session = request.getSession();
-//                                session.invalidate();
-//                                SecurityContextHolder.getContextHolderStrategy().getContext().setAuthentication(null);
-//                                SecurityContextHolder.getContextHolderStrategy().clearContext();
-//                            }
-//                        }).permitAll()
-//                );
-
-
-        //TODO : remember-Me
-//                .rememberMe(rememberMe -> rememberMe
-//                        .alwaysRemember(true)
-//                        .tokenValiditySeconds(3600)
-//                        .userDetailsService(userDetailsService())
-//                        .rememberMeParameter("remember")
-//                        .rememberMeCookieName("remember")
-//                        .key("security")
-//                )
-//        ;
+                .authenticationManager(authenticationManager)
+                .addFilterBefore(customFilter(http, authenticationManager), UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    public CustomAuthenticationFilter customFilter(HttpSecurity http, AuthenticationManager authenticationManager) {
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(http);
+        customAuthenticationFilter.setAuthenticationManager(authenticationManager);
+        return customAuthenticationFilter;
     }
 
     @Bean
